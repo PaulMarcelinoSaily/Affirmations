42855833de4498f674f0458df873e92b823910ac
marcelino04 <mrclno43@gmail.com>
2022-09-30 14:40:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,11 @@
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+}
